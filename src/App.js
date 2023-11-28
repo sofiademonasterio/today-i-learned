f8c1741cf44f23c9f2d6f369b8ae4b6d3d9a3811
Sofía de Monasterio <38118995+sofiademonasterio@users.noreply.github.com>
2023-11-27 23:29:08 -0300

    Add header and form components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import "./style.css";
 
 function App() {
+  const appTitle = "Today I Learned";
+
+  return (
+    <>
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function Header() {
   return (
     <header className="header">
       <div className="logo">
@@ -10,6 +36,18 @@ function App() {
       <button className="btn btn-large btn-open">Share a fact</button>
     </header>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Fact list</section>;
 }
 
 export default App;
