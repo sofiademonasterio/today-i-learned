99c3cdaf37369725b82f4e570810491944d99c8b
Sofía de Monasterio <38118995+sofiademonasterio@users.noreply.github.com>
2023-11-28 13:30:37 -0300

    Add initial facts and category filter components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import "./style.css";
 
+const initialFact = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    date: "2021-01-01",
+    category: "technology",
+    source: "https://opensource.fb.com/",
+  },
+  {
+    id: 2,
+    text: "Lisbon is the capital of Portugal",
+    category: "society",
+    date: "2021-01-01",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+  },
+];
+
+//DATA
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function App() {
   const appTitle = "Today I Learned";
 
@@ -42,12 +71,70 @@ function NewFactForm() {
   return <form className="fact-form">Fact form</form>;
 }
 
-function CategoryFilter() {
-  return <aside>Category filter</aside>;
+function FactList() {
+  const facts = initialFact;
+
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
+        ))}
+      </ul>
+    </section>
+  );
 }
 
-function FactList() {
-  return <section>Fact list</section>;
+function Fact({ fact }) {
+  return (
+    <li className="fact" key={fact.id}>
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)
+            .color,
+        }}
+      >
+        technology
+      </span>
+      <div className="vote-buttons">
+        <button>👍 24</button>
+        <button>🤯 9</button>
+        <button>⛔ 4</button>
+      </div>
+    </li>
+  );
+}
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        {CATEGORIES.map((category) => (
+          <Category key={category.name} category={category} />
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+function Category({ category }) {
+  return (
+    <li class="category">
+      <button
+        class="btn btn-category"
+        style={{ backgroundColor: category.color }}
+      >
+        {category.name}
+      </button>
+    </li>
+  );
 }
 
 export default App;
